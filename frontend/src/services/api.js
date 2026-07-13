8e32a18235e6df4826ae53c0dea62fdb05db4159
Syeda Nadiya <syedanadiya7@gmail.com>
2026-07-13 21:44:42 +0530

    Connect frontend to render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://ai-food-genie-app.onrender.com/api/v1",
 });
 
 export default API;
